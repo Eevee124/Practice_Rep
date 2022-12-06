29401ec4cfedc78b2ad2b5f8437d81bc9535b539
Eevee <evangelinezthoeny@gmail.com>
2022-12-06 18:01:54 +0100

    day 4

--- AoC/Day_4/day4.py
+++ AoC/Day_4/day4.py
@@ -1,37 +1,46 @@
 def first_task(filename):
     with open(filename) as f:
         file = f.readlines()
-    print(type(file))
+        assignement_pairs = [entry.strip() for entry in file]
+    #print(assignement_pairs)
     #file.replace('-', ',')
-    add = ''
-    zones = []
 
+    def contains(range_one, range_two):
+        start_first, end_first = map(int, range_one.split('-'))
+        start_second, end_second = map(int, range_two.split('-'))
+       # print(start_first, end_first, ":", start_second, end_second)
+        
+        return start_first <= start_second and end_first >= end_second
+
+    
     contained = 0
+    for pair in assignement_pairs:
+        first, second = pair.split(',')
+        #print(first, second)
 
-    for line in file:
-        #line = line.replace('-', ',')
-        for num in line:
-            if not num.isdigit():
-                zones.append(int(add))
-                add = ''
-                if num == line[-1]:
-                    if (zones[0] <= zones[2] and zones[1] >= zones[3]) or (zones[2] <= zones[0] and zones[1] >= zones[1]):
-                        contained += 1
-                    zones = []
-            else:
-                add += num
-    zones.append(int(add))
-    if (zones[0] <= zones[2] and zones[1] >= zones[3]) or (zones[2] <= zones[0] and zones[1] >= zones[1]):
-                        contained += 1
+        if contains(first, second) or contains(second, first): contained += 1
 
     return contained
 
 
 def second_task(filename):
-    pass
+    with open(filename) as f:
+        file = f.readlines()
+        assignement_pairs = [entry.strip() for entry in file]
+    
+    overlap = 0
+
+    for pair in assignement_pairs:
+        first, second = pair.split(',')
+        start_first, end_first = map(int, first.split('-'))
+        start_second, end_second = map(int, second.split('-'))
+
+        if start_first in range(start_second, end_second + 1) or end_first in range(start_second, end_second + 1) or start_second in range(start_first, end_first + 1) or end_second in range(start_first, end_first + 1): overlap += 1
+
+    return overlap
 
 print(first_task('test.txt'))
 print(first_task('input.txt'))
 
-#print(second_task('test.txt'))
-#print(second_task('input.txt'))
+print(second_task('test.txt'))
+print(second_task('input.txt'))
